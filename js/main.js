834f3ec49efe4b7048aede226b13ed00586eeb2a
Charlie Miller <charlie@thecharlah.com>
2015-10-07 17:38:44 -0400

    Worked on JS and rough CSS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 (function () {
 
-  console.log('It Works!');
+var templateString = $('#itemTemplate').text();
+
+var imageString = $('#imageTemplate').text();
+
+var templateFunction = _.template(templateString);
+
+var imageFunction = _.template(imageString);
+
+_.each(etsy.results, function (item) {
+  var itemHTML = templateFunction(item);
+    var imageHTML = imageFunction(item);
+  $('.productImages').append(imageHTML);
+  $('.searchResults').append(itemHTML);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }());
