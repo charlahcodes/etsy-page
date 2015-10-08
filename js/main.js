21bfd9933e75e60b076b3a7052bfd5d7eb3a6232
Charlie Miller <charlie@thecharlah.com>
2015-10-08 17:35:06 -0400

    Continued styling left sidebar

--- js/main.js
+++ js/main.js
@@ -2,32 +2,11 @@
 
 var templateString = $('#itemTemplate').text();
 
-var imageString = $('#imageTemplate').text();
-
 var templateFunction = _.template(templateString);
-
-var imageFunction = _.template(imageString);
 
 _.each(etsy.results, function (item) {
   var itemHTML = templateFunction(item);
-    var imageHTML = imageFunction(item);
-  $('.resultsContainer').append(imageHTML);
   $('.resultsContainer').append(itemHTML);
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }());
